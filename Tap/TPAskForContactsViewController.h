9f4a3d7b23c8629dfbb859ab75c3288efbb6ec6e
edwardlando <edwardlando8@gmail.com>
2014-07-05 23:09:43 -0700

    added screen for allowing contacts

---
+++ Tap/TPAskForContactsViewController.h
@@ -0,0 +1,15 @@
+//
+//  TPAskForContactsViewController.h
+//  Tap
+//
+//  Created by Edward Lando on 7/5/14.
+//  Copyright (c) 2014 Yagil Burowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPAskForContactsViewController : UIViewController
+
+- (IBAction)askForContacts:(id)sender;
+
+@end
---
+++ Tap/TPAskForContactsViewController.m
@@ -0,0 +1,27 @@
+//
+//  TPAskForContactsViewController.m
+//  Tap
+//
+//  Created by Edward Lando on 7/5/14.
+//  Copyright (c) 2014 Yagil Burowski. All rights reserved.
+//
+
+#import "TPAskForContactsViewController.h"
+
+@interface TPAskForContactsViewController ()
+
+@end
+
+@implementation TPAskForContactsViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+- (IBAction)askForContacts:(id)sender {
+}
+@end
